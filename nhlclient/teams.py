e9a29d3d7f66451ba79f9038c66bbee5e36b0879
nolanbradshaw <nolanbradshaw22@gmail.com>
2021-02-15 14:47:58 -0500

    added error handling for 404 and test cases for exceptions

--- nhlclient/teams.py
+++ nhlclient/teams.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import HTTPError
 from .constants import BASE_URL
 from .models import TeamModel, DivisionModel, ConferenceModel, VenueModel, PlayerModel
 
@@ -8,25 +9,31 @@ def get_by_id(id):
 
     Args:
         id (int): The id for the team.
+    
+    Raises:
+        ValueError: Could not find a team with the given id.
 
     Returns:
-        [TeamModel]: The TeamModel object.
+        TeamModel: The TeamModel object.
     """
-    
-    url = BASE_URL + f'/teams/{id}'
-    response = requests.get(url)
-    json = response.json()['teams'][0]
-    return TeamModel(json)
+    try:
+        url = BASE_URL + f'/teams/{id}'
+        resp = requests.get(url)
+        resp.raise_for_status()
+        
+        return TeamModel(resp.json()['teams'][0])
+    except HTTPError as e:
+        raise ValueError(f'Could not find a team with that id ({id}).')
 
 def get():
     """
     Get information for all teams.
 
     Returns:
-        [List of TeamModel]: A list of TeamModel objects.
+        List of TeamModel: A list of TeamModel objects.
     """
-    response = requests.get(BASE_URL + '/teams')
-    json = response.json()['teams']
+    resp = requests.get(BASE_URL + '/teams')
+    json = resp.json()['teams']
     team_list = [TeamModel(team) for team in json]
     
     return team_list
@@ -38,15 +45,21 @@ def get_roster_by_id(id):
     Args:
         id (int): The id for the team.
 
+    Raises:
+        ValueError: Could not find a team with the given id.
+
     Returns:
-        [List of PlayerModel]: A list of PlayerModel objects.
+        List of PlayerModel: A list of PlayerModel objects.
     """
+    try:
+        url = BASE_URL + f'/teams/{id}?expand=team.roster'
+        resp = requests.get(url)
+        resp.raise_for_status()
+        json = resp.json()['teams'][0]
     
-    url = BASE_URL + f'/teams/{id}?expand=team.roster'
-    response = requests.get(url)
-    json = response.json()['teams'][0]
-    
-    return [PlayerModel(player) for player in json['roster']['roster']]
+        return [PlayerModel(player) for player in json['roster']['roster']]
+    except HTTPError as e:
+        raise ValueError(f'Could not find a team with that id ({id}).')
 
 def get_roster_by_season(id, season):
     """
@@ -56,13 +69,20 @@ def get_roster_by_season(id, season):
         id (int): The id for the team.
         season (str): The season to retrieve the roster (Ex. 20132014).
 
+    Raises:
+        ValueError: Could not find team by id for the given season.
+
     Returns:
-        [List of PlayerModel]: A list of PlayerModel objects.
+        List of PlayerModel: A list of PlayerModel objects.
     """
-    
-    url = BASE_URL + f'/teams/{id}?expand=team.roster&season={season}'
-    response = requests.get(url)
-    json = response.json()['teams'][0]
-    return [PlayerModel(player) for player in json['roster']['roster']]
+    try:
+        url = BASE_URL + f'/teams/{id}?expand=team.roster&season={season}'
+        resp = requests.get(url)
+        resp.raise_for_status()
+        json = resp.json()['teams'][0]
+        
+        return [PlayerModel(player) for player in json['roster']['roster']]
+    except HTTPError as e:
+        raise ValueError(f'Could not find a team with that id ({id}) for the given season.')
 
     
--- test/test_teams.py
+++ test/test_teams.py
@@ -6,6 +6,12 @@ class Test_Teams(unittest.TestCase):
     def test_get_by_id(self):
         response = teams.get_by_id(1)
         self.assertIsInstance(response, TeamModel)
+        
+    def test_get_by_id_not_found(self):
+        """
+            Should raise a ValueError as no team with id 0 exists.
+        """
+        self.assertRaises(ValueError, teams.get_by_id, 0)
     
     def test_get(self):
         response = teams.get()
@@ -17,7 +23,19 @@ class Test_Teams(unittest.TestCase):
         self.assertIsInstance(response, list)
         assert isinstance(response[0], PlayerModel)
         
+    def test_roster_by_id_not_found(self):
+        """
+            Should raise a ValueError as no team with id 0 exists.
+        """
+        self.assertRaises(ValueError, teams.get_roster_by_id, 0)
+    
     def test_roster_by_season(self):
         response = teams.get_roster_by_season(1, '20132014')
         self.assertIsInstance(response, list)
         assert isinstance(response[0], PlayerModel)
+        
+    def test_roster_by_season_not_found(self):
+        """
+            Should raise a ValueError as no team with id 0 exists.
+        """
+        self.assertRaises(ValueError, teams.get_roster_by_season, 0, '20102011')
