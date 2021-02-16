d7a11312d7398efbcd80b8a6978d17218fa547be
nolanbradshaw <nolanbradshaw22@gmail.com>
2021-02-16 11:14:58 -0500

    added constants for team ids

--- test/test_teams.py
+++ test/test_teams.py
@@ -1,12 +1,13 @@
 import unittest
 from nhlclient import teams
+from nhlclient.constants import TEAMS
 from nhlclient.models.game import Game
 from nhlclient.models.player import Player
 from nhlclient.models.team import Team
 
 class TestTeams(unittest.TestCase):
     def test_get_by_id(self):
-        response = teams.get_by_id(1)
+        response = teams.get_by_id(TEAMS['TOR'])
         self.assertIsInstance(response, Team)
         
     def test_get_by_id_not_found(self):
@@ -21,7 +22,7 @@ class TestTeams(unittest.TestCase):
         assert isinstance(response[0], Team)
         
     def test_roster_by_id(self):
-        response = teams.get_roster_by_id(1)
+        response = teams.get_roster_by_id(TEAMS['TBL'])
         self.assertIsInstance(response, list)
         assert isinstance(response[0], Player)
         
@@ -32,7 +33,7 @@ class TestTeams(unittest.TestCase):
         self.assertRaises(ValueError, teams.get_roster_by_id, 0)
     
     def test_roster_by_season(self):
-        response = teams.get_roster_by_season(1, '20132014')
+        response = teams.get_roster_by_season(TEAMS['TOR'], '20132014')
         self.assertIsInstance(response, list)
         assert isinstance(response[0], Player)
         
@@ -40,10 +41,10 @@ class TestTeams(unittest.TestCase):
         """
             Should raise a ValueError as no team with id 0 exists.
         """
-        self.assertRaises(ValueError, teams.get_roster_by_season, 0, '20102011')
+        self.assertRaises(ValueError, teams.get_roster_by_season, TEAMS['MMR'], '20102011')
         
     def test_get_last_game(self):
-        response = teams.get_last_game(1)
+        response = teams.get_last_game(TEAMS['EDM'])
         self.assertIsInstance(response, Game)
         
     def test_get_last_game_not_found(self):
@@ -53,7 +54,7 @@ class TestTeams(unittest.TestCase):
         self.assertRaises(ValueError, teams.get_last_game, 0)
         
     def test_get_next_game(self):
-        response = teams.get_next_game(1)
+        response = teams.get_next_game(TEAMS['VAN'])
         self.assertIsInstance(response, Game)
     
     def test_get_next_game_not_found(self):
